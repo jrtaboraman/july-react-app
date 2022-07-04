ed96150523baddeee7a19db61dbd01caf84b2fea
jrtaboraman <jr.taboraman@gmail.com>
2022-07-04 21:39:18 +0800

    First commit

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Home = () => (
+    <React.Fragment>
+    <h1>
+        Welcome to my blog!
+    </h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi ipsam necessitatibus fuga, perferendis aliquam quam nesciunt fugiat voluptas neque ex soluta repellendus, omnis totam suscipit! Repellendus, officiis! Ea, odio! Impedit!
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, id asperiores odio iure nulla quasi officia quibusdam nisi nesciunt sed ad, neque ipsam, ipsum numquam quas aut doloribus reprehenderit voluptatem?
+    </p>
+    </React.Fragment>
+
+);
+
+export default Home
